dc67da55a1aec65175d69dff19fcaee2f20bb94a
Benjamin Cheng <bencheng200@gmail.com>
2021-01-05 22:27:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,14 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-get '/' do
-  erb :user_input
-end
+  get '/' do
+    erb :user_input
+  end
 
-post '/piglatinize' do
-  pl = PigLatinizer.new
-  @piglatin = pl.to_pig_latin(params[:user_phrase])
-  erb :results
-end
+  post '/piglatinize' do
+    pl = PigLatinizer.new
+    @piglatin = pl.piglatinize(params[:user_phrase])
+    erb :results
+  end
 
 end
